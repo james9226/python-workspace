0e6eec069149b297f5464cbed492edf1d3eb9731
James Neale <42097394+james9226@users.noreply.github.com>
2022-10-02 02:06:37 +0100

    tester

--- python_workspace/demo.py
+++ python_workspace/demo.py
@@ -29,6 +29,8 @@ hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
+print('asufhasoifsaofhosaihfiosahfioashfiosanfiosanfoisabfisabfiosanfiosanfiosanfiosaniofsaifosbofasbf')
+
 hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
