b1f792603f003de85ce53e9368ddf99e49c15d9d
James Neale <42097394+james9226@users.noreply.github.com>
2023-03-19 13:23:23 +0000

    Add basic rate limiting

--- src/authentication_service/db/queries.py
+++ src/authentication_service/db/queries.py
@@ -1,5 +1,7 @@
+import datetime
 from src.authentication_service.db.firestore import load_db
 from src.models.user import User
+import streamlit as st
 
 
 def get_whitelisted_users() -> dict:
@@ -18,3 +20,33 @@ def query_user(email: str) -> User:
     doc = db.collection("users").document(email).get()
 
     return User(**doc.to_dict())
+
+
+@st.cache_data(ttl=60)
+def query_number_request_attempts() -> int:
+    db = load_db()
+    collection = (
+        db.collection("login_request_attempts")
+        .document(datetime.datetime.now().strftime("%Y-%m-%d"))
+        .collection("requests")
+    )
+    snapshot = collection.count().get()[0]
+
+    return snapshot[0].value
+
+
+def update_request_attempts(email: str, success: bool, reason: str = ""):
+    db = load_db()
+    collection = (
+        db.collection("login_request_attempts")
+        .document(datetime.datetime.now().strftime("%Y-%m-%d"))
+        .collection("requests")
+    )
+    collection.document().set(
+        {
+            "email": email,
+            "event_time": datetime.datetime.now(),
+            "login_successful": success,
+            "reason": reason,
+        }
+    )
--- src/authentication_service/login.py
+++ src/authentication_service/login.py
@@ -1,5 +1,9 @@
 import bcrypt
-from src.authentication_service.db.queries import query_user
+from src.authentication_service.db.queries import (
+    query_user,
+    query_number_request_attempts,
+    update_request_attempts,
+)
 
 
 def secure_hash(password: str) -> str:
@@ -16,17 +20,25 @@ def check_password(password: str, hashed_password: str) -> bool:
 
 def authenticate_login(email: str, password: str) -> bool:
 
+    print(query_number_request_attempts())
+    if query_number_request_attempts() > 1000:
+        return False
+
     try:
         user_data = query_user(email)
     except:
+        update_request_attempts(email, False, "EMAIL_NOT_FOUND_IN_DB")
         return False
 
     authenticated = check_password(password, user_data.hashed_password)
 
     if not authenticated:
+        update_request_attempts(email, False, "FAILED_PASSWORD_AUTHENTICATION")
         return False
 
     if not user_data.is_enabled:
+        update_request_attempts(email, False, "USER_IS_DISABLED")
         return False
 
+    update_request_attempts(email, True, "USER_ENABLED_AND_PASSWORD_AUTHENTICATED")
     return True
